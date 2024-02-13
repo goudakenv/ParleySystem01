7663ac1537ee1ea398bb317e5dbd0a02cbb46972
ota <ota@sample.com>
2024-02-13 09:29:49 +0900

    repair

--- ParleySystem01/src/main/resources/static/js/button-action.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.delete')
-	.forEach(button =>  {
-		button.addEventListener('click', (e) => {
-			if(!confirm('本当に削除してよろしいですか？')) {
-				e.preventDefault();
-			}
-		});
-	});
